a2a2492a610678522b4e4cd98f9daaa0055b8805
Kali <Kali@DESKTOP-73ONRB5>
2025-02-11 12:50:36 +0100

    Добавьте файлы проекта.

---
+++ StatsLoader/AppConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatsLoader
+{
+    internal class AppConfig
+    {
+
+
+        public enum ApiPlatform
+        {
+            Wildberries
+        }
+
+
+    }
+
+    internal class ConnectionString
+    {
+        public string HOST { get; private set; }
+        public string DB { get; private set; }
+        public string USERNAME { get; private set; }
+        public string PASSWORD { get; private set; }
+        public string ConnString { get; private set; }
+
+        public ConnectionString()
+        {
+            HOST = "ep-winter-mouse-a9l6j2be-pooler.gwc.azure.neon.tech";
+            DB = "neondb";
+            USERNAME = "neondb_owner";
+            PASSWORD = "npg_kA4pbgFl0tyY";
+
+            ConnString = $"Host={HOST};Database={DB};Username={USERNAME};Password={PASSWORD}";
+        }
+    }
+}
